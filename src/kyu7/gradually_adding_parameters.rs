00ce832ef170dd6ecf0223135c6cb7ee379d9289
Stallmanifold <stallmanifold@gmail.com>
2017-08-17 03:33:36 +0000

    Update kata.

--- src/kyu7/gradually_adding_parameters.rs
+++ src/kyu7/gradually_adding_parameters.rs
@@ -2,7 +2,7 @@
 
 
 fn add(args: &[i64]) -> i64 {
-    args.iter().enumerate().fold(0, |acc, (i, v)| { acc + (*v)*((i as i64)+1) })
+    args.iter().enumerate().fold(0, |acc, (i, v)| { acc + (v)*((i as i64)+1) })
 }
 
 #[cfg(test)]
