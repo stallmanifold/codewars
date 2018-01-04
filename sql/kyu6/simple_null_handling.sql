6199d4bf226bda5ec3ebf4482979148ddba2ccb4
Stallmanifold <stallmanifold@gmail.com>
2018-01-04 02:58:31 +0000

    SQL exercises.

---
+++ sql/kyu6/simple_null_handling.sql
@@ -0,0 +1,14 @@
+SELECT 
+  id,
+  COALESCE(
+    NULLIF(name, ''), '[product name not found]'
+  ) as name,
+  price,
+  COALESCE(
+    NULLIF(card_name, ''), '[card name not found]'
+  ) as card_name,
+  card_number,
+  transaction_date
+FROM eusales
+WHERE price IS NOT NULL
+  AND price > 50.00
